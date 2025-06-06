1630d17ccb22eb17fcf3253aaa7115501319f2e8
wwwaaa123122 <wwwaaa123122@outlook.com>
2025-06-06 10:19:35 +0800

    Update App.vue

--- mail-vue/src/App.vue
+++ mail-vue/src/App.vue
@@ -6,7 +6,7 @@
 
 <style>
 .app-container {
-  background-image: url('@/assets/bg.jpg');
+  background-image: url('https://wwwaaa123122.cn-nb1.rains3.com/img/Image_1748831276428.png');
   background-size: cover;
   background-position: center;
   min-height: 100vh;
