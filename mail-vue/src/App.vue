6a512e0ef577fd104c9ec89e3cf3f5f93372e8e6
wwwaaa123122 <wwwaaa123122@outlook.com>
2025-06-06 10:16:37 +0800

    Update App.vue

--- mail-vue/src/App.vue
+++ mail-vue/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <router-view/>
+  <div class="app-container">
+    <router-view/>
+  </div>
 </template>
-<script setup>
-
-</script>
 
 <style>
-
+.app-container {
+  background-image: url('@/assets/bg.jpg');
+  background-size: cover;
+  background-position: center;
+  min-height: 100vh;
+}
 </style>
